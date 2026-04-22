f6fe1bbff9105026a81e4a657dc4dc78f3748a2b
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2026-04-22 10:11:39 +0800

    feat: update Page component layout to include PlanningPain, HowItWorks, Features, Testimonials, and Footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { Features } from "@/components/sections/features"
+import { Footer } from "@/components/sections/footer"
 import { Hero } from "@/components/sections/hero"
+import { HowItWorks } from "@/components/sections/how-it-works"
+import { PlanningPain } from "@/components/sections/planning-pain"
+import { Testimonials } from "@/components/sections/testimonials"
 
 export default function Page() {
   return (
-    <main>
-      <Hero />
-    </main>
+    <>
+      <main>
+        <Hero />
+        <PlanningPain />
+        <HowItWorks />
+        <Features />
+        <Testimonials />
+      </main>
+      <Footer />
+    </>
   )
 }
